6f6f9d1764e7af6a35795d5ff1bfc610804ff629
Maitreya29 <maitreyapatni30@gmail.com>
2021-01-18 17:58:16 +0530

    Bring back some of our customisations

--- src/com/raven/lair/fragments/TrafficIndicators.java
+++ src/com/raven/lair/fragments/TrafficIndicators.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2019 The Dirty Unicorns Project
+ * Copyright (C) 2020 NezukoOS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,80 +18,189 @@ package com.nezuko.extra.fragments;
 
 import android.content.ContentResolver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
-import androidx.preference.*;
 
-import com.android.internal.logging.nano.MetricsProto;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
 
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.Indexable;
+import com.android.settingslib.search.SearchIndexable;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
-import com.android.settingslib.search.SearchIndexable;
 
-import android.util.Log;
-import android.text.TextUtils;
-import android.view.View;
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.extra.settings.preferences.CustomSeekBarPreference;
+import com.extra.settings.preferences.SecureSettingListPreference;
+import com.extra.settings.preferences.SecureSettingSwitchPreference;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Collections;
-import java.util.Locale;
-
-import com.nezuko.extra.preferences.CustomSeekBarPreference;
 
 @SearchIndexable
-public class TrafficIndicators extends SettingsPreferenceFragment
-        implements Preference.OnPreferenceChangeListener, Indexable {
+public class TrafficIndicators extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener, Indexable {
 
-    private CustomSeekBarPreference mThreshold;
-    private SwitchPreference mNetMonitor;
-    private SwitchPreference mNetArrow;
+    private static final String TAG = "NetworkTrafficSettings";
+
+    private CustomSeekBarPreference mNetTrafficAutohideThreshold;
+    private CustomSeekBarPreference mNetTrafficRefreshInterval;
+    private SecureSettingListPreference mNetTrafficLocation;
+    private SecureSettingListPreference mNetTrafficMode;
+    private SecureSettingListPreference mNetTrafficUnits;
+    private SecureSettingSwitchPreference mNetTrafficAutohide;
+    private SecureSettingSwitchPreference mNetTrafficHideArrow;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.traffic_indicators);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        int location = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.NETWORK_TRAFFIC_LOCATION, 0, UserHandle.USER_CURRENT);
+        mNetTrafficLocation = (SecureSettingListPreference) findPreference("network_traffic_location");
+		mNetTrafficLocation.setValue(String.valueOf(location));
+        mNetTrafficLocation.setSummary(mNetTrafficLocation.getEntry());
+        mNetTrafficLocation.setOnPreferenceChangeListener(this);
+
+        int mode = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.NETWORK_TRAFFIC_MODE, 0, UserHandle.USER_CURRENT);
+        mNetTrafficMode = (SecureSettingListPreference) findPreference("network_traffic_mode");
+        mNetTrafficMode.setValue(String.valueOf(mode));
+        mNetTrafficMode.setSummary(mNetTrafficMode.getEntry());
+        mNetTrafficMode.setOnPreferenceChangeListener(this);
+
+        int thresholdValue = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, 1, UserHandle.USER_CURRENT);
+        mNetTrafficAutohideThreshold = (CustomSeekBarPreference) findPreference("network_traffic_autohide_threshold");
+        mNetTrafficAutohideThreshold.setValue(thresholdValue);
+        mNetTrafficAutohideThreshold.setOnPreferenceChangeListener(this);
+
+        int intervalValue = Settings.Secure.getInt(resolver,
+                Settings.Secure.NETWORK_TRAFFIC_REFRESH_INTERVAL, 2);
+        mNetTrafficRefreshInterval =
+                (CustomSeekBarPreference) findPreference("network_traffic_refresh_interval");
+        mNetTrafficRefreshInterval.setValue(intervalValue);
+        mNetTrafficRefreshInterval.setOnPreferenceChangeListener(this);
+
+        int unitValue = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.NETWORK_TRAFFIC_UNITS, 0, UserHandle.USER_CURRENT);
+        mNetTrafficUnits = (SecureSettingListPreference) findPreference("network_traffic_units");
+        mNetTrafficUnits.setValue(String.valueOf(unitValue));
+        mNetTrafficUnits.setSummary(mNetTrafficUnits.getEntry());
+        mNetTrafficUnits.setOnPreferenceChangeListener(this);
+
+        mNetTrafficAutohide = (SecureSettingSwitchPreference) findPreference("network_traffic_autohide");
+        boolean autoHide = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.NETWORK_TRAFFIC_AUTOHIDE, 0, UserHandle.USER_CURRENT) != 0;
+        mNetTrafficAutohide.setChecked(autoHide);
+        mNetTrafficAutohide.setOnPreferenceChangeListener(this);
+
+        mNetTrafficHideArrow = (SecureSettingSwitchPreference) findPreference("network_traffic_hidearrow");
+        boolean hideArrows = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.NETWORK_TRAFFIC_HIDEARROW, 0, UserHandle.USER_CURRENT) != 0;
+        mNetTrafficHideArrow.setChecked(hideArrows);
+        mNetTrafficHideArrow.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getContext().getContentResolver();
+        if (preference == mNetTrafficLocation) {
+            int location = Integer.valueOf((String) newValue);
+            int index = mNetTrafficLocation.findIndexOfValue((String) newValue);
+            Settings.Secure.putIntForUser(getActivity().getContentResolver(),
+                    Settings.Secure.NETWORK_TRAFFIC_LOCATION,
+                    location, UserHandle.USER_CURRENT);
+            mNetTrafficLocation.setSummary(mNetTrafficLocation.getEntries()[index]);
+            // Preference enablement checks
+            mNetTrafficMode.setEnabled(netTrafficEnabled());
+            mNetTrafficHideArrow.setEnabled(netTrafficEnabled());
+            mNetTrafficAutohideThreshold.setEnabled(netTrafficEnabled());
+            mNetTrafficRefreshInterval.setEnabled(netTrafficEnabled());
+            mNetTrafficUnits.setEnabled(netTrafficEnabled());
+            mNetTrafficAutohide.setEnabled(netTrafficEnabled());
+            return true;
+        } else if (preference == mNetTrafficMode) {
+            int mode = Integer.valueOf((String) newValue);
+            int index = mNetTrafficMode.findIndexOfValue((String) newValue);
+            Settings.Secure.putIntForUser(getActivity().getContentResolver(),
+                    Settings.Secure.NETWORK_TRAFFIC_MODE,
+                    mode, UserHandle.USER_CURRENT);
+            mNetTrafficMode.setSummary(mNetTrafficMode.getEntries()[index]);
+            mNetTrafficRefreshInterval.setEnabled(netTrafficEnabled());
+            return true;
+        } else if (preference == mNetTrafficUnits) {
+            int mode = Integer.valueOf((String) newValue);
+            int index = mNetTrafficUnits.findIndexOfValue((String) newValue);
+            Settings.Secure.putIntForUser(getActivity().getContentResolver(),
+                    Settings.Secure.NETWORK_TRAFFIC_UNITS,
+                    mode, UserHandle.USER_CURRENT);
+            mNetTrafficUnits.setSummary(mNetTrafficUnits.getEntries()[index]);
+            mNetTrafficRefreshInterval.setEnabled(netTrafficEnabled());
+            return true;
+        } else if (preference == mNetTrafficAutohideThreshold) {
+            int thresholdValue = (Integer) newValue;
+            Settings.Secure.putIntForUser(getContentResolver(),
+                    Settings.Secure.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD,
+                    thresholdValue, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mNetTrafficRefreshInterval) {
+            int interval = (Integer) newValue;
+            Settings.Secure.putIntForUser(getContentResolver(),
+                    Settings.Secure.NETWORK_TRAFFIC_REFRESH_INTERVAL,
+                    interval, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mNetTrafficAutohide) {
+            boolean autoHide = (Boolean) newValue;
+            Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.NETWORK_TRAFFIC_AUTOHIDE, autoHide ? 1 : 0, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mNetTrafficHideArrow) {
+            boolean hideArrows = (Boolean) newValue;
+            Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.NETWORK_TRAFFIC_HIDEARROW, hideArrows ? 1 : 0, UserHandle.USER_CURRENT);
+            return true;
+        }
         return false;
     }
 
+    private boolean netTrafficEnabled() {
+        final ContentResolver resolver = getActivity().getContentResolver();
+        return Settings.Secure.getInt(resolver,
+                Settings.Secure.NETWORK_TRAFFIC_LOCATION, 0) != 0;
+	}
+
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.NEZUKO;
+        return MetricsProto.MetricsEvent.EXTRA;
     }
 
     public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider() {
-                @Override
-                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
-                        boolean enabled) {
-                    ArrayList<SearchIndexableResource> result =
-                            new ArrayList<SearchIndexableResource>();
+        new BaseSearchIndexProvider() {
+            @Override
+            public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                    boolean enabled) {
+                final ArrayList<SearchIndexableResource> result = new ArrayList<>();
+                final SearchIndexableResource sir = new SearchIndexableResource(context);
+                sir.xmlResId = R.xml.traffic_indicators;
+                result.add(sir);
+                return result;
+            }
 
-                    SearchIndexableResource sir = new SearchIndexableResource(context);
-                    sir.xmlResId = R.xml.traffic_indicators;
-                    result.add(sir);
-                    return result;
-                }
-
-                @Override
-                public List<String> getNonIndexableKeys(Context context) {
-                    List<String> keys = super.getNonIndexableKeys(context);
-                    return keys;
-        }
+            @Override
+            public List<String> getNonIndexableKeys(Context context) {
+                final List<String> keys = super.getNonIndexableKeys(context);
+                return keys;
+            }
     };
 }
--- src/com/raven/lair/tabs/Lockscreen.java
+++ src/com/raven/lair/tabs/Lockscreen.java
@@ -31,16 +31,38 @@ import com.android.internal.logging.nano.MetricsProto;
 public class Lockscreen extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
+    private static final String FINGERPRINT_VIB = "fingerprint_success_vib";
+
+    private FingerprintManager mFingerprintManager;
+    private SwitchPreference mFingerprintVib;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.lockscreen);
 
+        mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+        mFingerprintVib = (SwitchPreference) findPreference(FINGERPRINT_VIB);
+        if (!mFingerprintManager.isHardwareDetected()){
+            prefScreen.removePreference(mFingerprintVib);
+        } else {
+            mFingerprintVib.setChecked((Settings.System.getInt(getContentResolver(),
+                    Settings.System.FINGERPRINT_SUCCESS_VIB, 1) == 1));
+            mFingerprintVib.setOnPreferenceChangeListener(this);
+        }
+
     }
 
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+
+        if (preference == mFingerprintVib) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.FINGERPRINT_SUCCESS_VIB, value ? 1 : 0);
+            return true;
+        }
+
         return false;
     }
 
